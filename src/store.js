2be10db2264265c07133a6a048007d5cadd2fd8d
noamtarshish <noamtnoam@gmail.com>
2025-06-18 11:54:15 +0300

    Updated files

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import { reactive } from 'vue';
 const store = reactive({
   username: localStorage.getItem('username'),
   server_domain: "http://localhost:3000",
+  // server_domain: "https://shay-noam."
 
   login(username) {
     localStorage.setItem('username', username);
